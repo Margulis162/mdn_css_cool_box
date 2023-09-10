1a4403fab839be5cc032f1a2632870df70834cfc
Margulis162 <markmargulis@proton.me>
2023-09-09 20:51:38 -0400

    added a snippet to change bg color of the boxes at a random one on click

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // var
 const box = document.getElementsByClassName("cool_box");
-console.log(box);
+
 let shadow ="";
 // f(x)
 function long_shadow(){
@@ -11,12 +11,26 @@ function long_shadow(){
        
     }
     shadow = shadow.slice(0, -1);
-    console.log(shadow);
+   
     box[0].style.boxShadow = shadow;
     box[1].style.boxShadow = shadow;
     box[2].style.boxShadow = shadow;
 }
 
 
+function random(min, max) {
+    const num = Math.floor(Math.random() * (max - min + 1)) + min;
+    return num;
+  }
+
+  for (let i = 0; i < box.length; i++){
+    box[i].addEventListener('click', (e)=>{
+        box[i].style.backgroundColor =`rgb(${random(0,255)},${random(0,255)}, ${random(0,255)})`;
+    })
+  }
+
+
 // flow
 long_shadow();
+
+
